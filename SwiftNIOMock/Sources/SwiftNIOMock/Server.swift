6a25a0bbf65aa7ae3a48ed54a2a9c929b15c61cc
Ilya Puchka <ilyapuchka@gmail.com>
2018-12-20 17:00:31 +0000

    implemented router and other middlewares

--- SwiftNIOMock/Sources/SwiftNIOMock/Server.swift
+++ SwiftNIOMock/Sources/SwiftNIOMock/Server.swift
@@ -16,15 +16,18 @@ open class Server {
     var serverChannel: Channel!
     public let port: Int
 
-    public init(port: Int) {
+    public init(port: Int, router: @escaping Middleware) {
         self.port = port
 
         self.bootstrap = ServerBootstrap(group: group)
             .serverChannelOption(ChannelOptions.backlog, value: 256)
             .serverChannelOption(ChannelOptions.socket(SocketOptionLevel(SOL_SOCKET), SO_REUSEADDR), value: 1)
             .childChannelInitializer { channel in
-                channel.pipeline.configureHTTPServerPipeline(withErrorHandling: true).then {
-                    channel.pipeline.add(handler: HTTPHandler())
+                channel.pipeline.configureHTTPServerPipeline(withErrorHandling: true)
+                    .then {
+                        channel.pipeline.add(handler: HTTPHandler(router: router))
+                    }.then {
+                        channel.pipeline.add(handler: HTTPResponseCompressor())
                 }
             }
             .childChannelOption(ChannelOptions.socket(IPPROTO_TCP, TCP_NODELAY), value: 1)
@@ -51,57 +54,24 @@ open class Server {
         print("Server released")
     }
 
-    class HTTPHandler: ChannelInboundHandler {
-        typealias InboundIn = HTTPServerRequestPart
+}
 
-        var state: State = .idle
+extension Server {
+    public class HTTPHandler: ChannelInboundHandler {
+        public typealias InboundIn = HTTPServerRequestPart
 
-        enum State {
-            case idle
-            case receivingRequest(HTTPRequestHead, ByteBuffer?)
-            case sendingResponse
+        private var state: State = .idle
+        let router: Middleware
 
-            mutating func requestReceived(head: HTTPRequestHead) {
-                guard case .idle = self else {
-                    precondition(false, "Invalid state for \(#function): \(self)")
-                }
-                print("Received request: ", head)
-                self = .receivingRequest(head, nil)
-            }
-
-            mutating func bodyReceived(buffer: ByteBuffer) {
-                var body = buffer
-                guard case .receivingRequest(let header, var buffer) = self else {
-                    precondition(false, "Invalid state for \(#function): \(self)")
-                }
-                if buffer == nil {
-                    buffer = body
-                } else {
-                    buffer?.write(buffer: &body)
-                }
-                self = .receivingRequest(header, buffer)
-            }
-
-            mutating func requestComplete() -> (HTTPRequestHead, ByteBuffer?)  {
-                guard case let .receivingRequest(header, buffer) = self else {
-                    precondition(false, "Invalid state for \(#function): \(self)")
-                }
-                if var buffer = buffer {
-                    print("Received body: \(buffer.readString(length: buffer.readableBytes) ?? "nil")")
-                }
-                self = .sendingResponse
-                return (header, buffer)
-            }
-
-            mutating func responseComplete() {
-                guard case .sendingResponse = self else {
-                    precondition(false, "Invalid state for response complete: \(self)")
-                }
-                self = .idle
-            }
+        init(router: @escaping Middleware) {
+            self.router = router
         }
 
-        func channelRead(ctx: ChannelHandlerContext, data: NIOAny) {
+        public func channelRead(ctx: ChannelHandlerContext, data: NIOAny) {
+            defer {
+                ctx.fireChannelRead(data)
+            }
+
             switch unwrapInboundIn(data) {
             case let .head(head):
                 state.requestReceived(head: head)
@@ -110,23 +80,205 @@ open class Server {
             case .end:
                 let (head, buffer) = state.requestComplete()
 
-                var headers = HTTPHeaders()
-                if let buffer = buffer {
-                    headers.add(name: "Content-Length", value: "\(buffer.readableBytes)")
-                    _ = ctx.channel.write(HTTPServerResponsePart.head(
-                        HTTPResponseHead(version: head.version, status: .ok, headers: headers))
-                    )
-                    _ = ctx.channel.write(HTTPServerResponsePart.body(.byteBuffer(buffer)))
-                } else {
-                    _ = ctx.channel.write(HTTPServerResponsePart.head(
-                        HTTPResponseHead(version: head.version, status: .ok))
-                    )
+                var httpBody: Data?
+                if var body = buffer {
+                    httpBody = body.readString(length: body.readableBytes)?.data(using: .utf8)
                 }
-                _ = ctx.channel.writeAndFlush(HTTPServerResponsePart.end(nil)).then {
-                    ctx.channel.close()
+
+                let request = Request(head: head, body: httpBody, ctx: ctx)
+
+                var responseBuffer = ctx.channel.allocator.buffer(capacity: 0)
+
+                let eventLoop = ctx.channel.eventLoop
+                let response = Response(eventLoop: eventLoop)
+
+                router(request, response) {
+                    eventLoop.execute {
+                        _ = ctx.channel.write(HTTPServerResponsePart.head(
+                            HTTPResponseHead(version: head.version, status: response.statusCode, headers: response.headers))
+                        )
+                        _ = response.body
+                            .flatMap { String(data: $0, encoding: .utf8) }
+                            .flatMap { responseBuffer.write(string: $0) }
+
+                        _ = ctx.channel.write(HTTPServerResponsePart.body(.byteBuffer(responseBuffer)))
+
+                        _ = ctx.channel.writeAndFlush(HTTPServerResponsePart.end(nil)).then {
+                            ctx.channel.close()
+                        }
+                        self.state.responseComplete()
+                    }
                 }
-                state.responseComplete()
             }
+        }
+    }
+}
+
+extension Server.HTTPHandler {
+    enum State {
+        case idle
+        case ignoringRequest
+        case receivingRequest(HTTPRequestHead, ByteBuffer?)
+        case sendingResponse
+
+        mutating func requestReceived(head: HTTPRequestHead) {
+            guard case .idle = self else {
+                precondition(false, "Invalid state for \(#function): \(self)")
+            }
+            print("Received request: ", head)
+            self = .receivingRequest(head, nil)
+        }
+
+        mutating func bodyReceived(buffer: ByteBuffer) {
+            var body = buffer
+            guard case .receivingRequest(let header, var buffer) = self else {
+                precondition(false, "Invalid state for \(#function): \(self)")
+            }
+            if buffer == nil {
+                buffer = body
+            } else {
+                buffer?.write(buffer: &body)
+            }
+            self = .receivingRequest(header, buffer)
+        }
+
+        mutating func requestComplete() -> (HTTPRequestHead, ByteBuffer?)  {
+            guard case let .receivingRequest(header, buffer) = self else {
+                precondition(false, "Invalid state for \(#function): \(self)")
+            }
+            if var buffer = buffer {
+                print("Received body: \(buffer.readString(length: buffer.readableBytes) ?? "nil")")
+            }
+            self = .sendingResponse
+            return (header, buffer)
+        }
+
+        mutating func responseComplete() {
+            guard case .sendingResponse = self else {
+                precondition(false, "Invalid state for response complete: \(self)")
+            }
+            self = .idle
+        }
+    }
+}
+
+extension Server.HTTPHandler {
+    public struct Request {
+        public let head: HTTPRequestHead
+        public let body: Data?
+        public let ctx: ChannelHandlerContext
+
+        public init(head: HTTPRequestHead, body: Data?, ctx: ChannelHandlerContext) {
+            self.head = head
+            self.body = body
+            self.ctx = ctx
+        }
+    }
+}
+
+extension Server.HTTPHandler {
+    public class Response {
+        var eventLoop: EventLoop
+        var state: State = .idle
+        public private(set) var statusCode: HTTPResponseStatus = .ok
+        public private(set) var headers: HTTPHeaders = HTTPHeaders()
+        public private(set) var body: Data?
+
+        init(eventLoop: EventLoop) {
+            self.eventLoop = eventLoop
+        }
+
+        public func start(_ statusCode: HTTPResponseStatus) -> Void {
+            eventLoop.execute {
+                self.statusCode = statusCode
+            }
+        }
+
+        public func setHeaders(_ headers: HTTPHeaders) -> Void {
+            eventLoop.execute {
+                headers.forEach { self.headers.replaceOrAdd(name: $0, value: $1) }
+            }
+        }
+
+        public func sendBody(_ data: Data) -> Void {
+            eventLoop.execute {
+                var body = self.body ?? Data()
+                body.append(data)
+                self.body = body
+            }
+        }
+
+        public func sendJSON<T: Encodable>(_ statusCode: HTTPResponseStatus, value: T) -> Void {
+            start(statusCode)
+            setHeaders(HTTPHeaders([("Content-Type", "application/json; charset=utf-8")]))
+            try! sendBody(JSONEncoder().encode(value))
+        }
+
+        public func sendString(_ statusCode: HTTPResponseStatus, value: String) -> Void {
+            start(statusCode)
+            setHeaders(HTTPHeaders([("Content-Type", "text/html; charset=utf-8")]))
+            sendBody(value.data(using: .utf8)!)
+        }
+    }
+}
+
+extension URLRequest {
+    public init(_ request: Server.HTTPHandler.Request) {
+        let url = URL(string: request.head.uri)!
+
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = "\(request.head.method)"
+        urlRequest.allHTTPHeaderFields = request.head.headers.reduce(into: [:]) { (headers, pair) in
+            headers[pair.name] = pair.value
+        }
+        urlRequest.httpBody = request.body
+        self = urlRequest
+    }
+}
+
+public typealias Middleware = (
+    _ request: Server.HTTPHandler.Request,
+    _ response: Server.HTTPHandler.Response,
+    _ next: @escaping () -> Void
+) -> Void
+
+/// Middleware that can redirect requests to other middlewares
+public func router(route: @escaping (Server.HTTPHandler.Request) -> Middleware?, notFound: @escaping Middleware) -> Middleware {
+    return { request, response, next in
+        (route(request) ?? notFound)(request, response, next)
+    }
+}
+
+/// Middleware that allows to redirect incomming request and intercept responses
+public func redirect(
+    session: URLSession = URLSession.shared,
+    request override: @escaping (Server.HTTPHandler.Request) -> Server.HTTPHandler.Request,
+    body: @escaping (Data) -> Data = { $0 }) -> Middleware {
+    return { request, response, next in
+        let task = session.dataTask(with: URLRequest(override(request))) { data, urlResponse, error in
+            guard let urlResponse = urlResponse as? HTTPURLResponse else {
+                next()
+                return
+            }
+            response.start(HTTPResponseStatus(statusCode: urlResponse.statusCode))
+            var headers = HTTPHeaders(urlResponse.allHeaderFields.map { ("\($0.key)", "\($0.value)") })
+            // compressor will add this header itself,
+            // so they may be duplicated and decoding on the client side will fail
+            headers.remove(name: "Content-Encoding")
+
+            response.setHeaders(headers)
+            data.map(body).map(response.sendBody)
+            next()
+        }
+        task.resume()
+    }
+}
+
+/// Middleware that delay another middleware
+public func delay(_ delay: TimeAmount, middleware: @escaping Middleware) -> Middleware {
+    return { request, response, next in
+        request.ctx.eventLoop.scheduleTask(in: delay) {
+            middleware(request, response, next)
         }
     }
 }
--- SwiftNIOMockExample/SwiftNIOMockExampleUITests/SwiftNIOMockExampleUITests.swift
+++ SwiftNIOMockExample/SwiftNIOMockExampleUITests/SwiftNIOMockExampleUITests.swift
@@ -11,11 +11,32 @@ import SwiftNIOMock
 
 class SwiftNIOMockExampleUITests: XCTestCase {
     var server: Server!
+    let notFound = SwiftNIOMock.redirect(
+        session: URLSession.shared,
+        request: { (request) in
+            var head = request.head
+            head.uri = "https://postman-echo.com/\(String(describing: request.head.method).lowercased())"
+            return Server.HTTPHandler.Request(head: head, body: request.body, ctx: request.ctx)
+    }, body: { data in
+        let string = String.init(data: data, encoding: .utf8)!
+        return ("This response was intercepted!\r\n" + string).data(using: .utf8)!
+    })
 
     override func setUp() {
         continueAfterFailure = false
         XCUIApplication().launch()
-        server = Server(port: 8080)
+
+        server = Server(port: 8080, router:
+            SwiftNIOMock.router(route: { request in
+                guard case .GET = request.head.method, request.head.uri == "/helloworld" else {
+                    return nil
+                }
+                return { (request, response, next) in
+                    response.sendString(.ok, value: "Hello world!")
+                    next()
+                }
+            }, notFound: notFound)
+        )
         try! server.start()
     }
 
@@ -24,11 +45,10 @@ class SwiftNIOMockExampleUITests: XCTestCase {
         server = nil
     }
 
-    func testExample() {
+    func testGET() {
         let exp = expectation(description: "Recieved response")
         var request = URLRequest(url: URL(string: "http://localhost:8080")!)
-        request.httpMethod = "POST"
-        request.httpBody = "Hello world".data(using: .utf8)
+        request.httpMethod = "GET"
         URLSession.shared.dataTask(with: request) { (data, response, error) in
             print(String(data: data ?? Data(), encoding: .utf8) ?? "nil")
             print(response ?? "nil")
@@ -37,6 +57,58 @@ class SwiftNIOMockExampleUITests: XCTestCase {
                 exp.fulfill()
             }
         }.resume()
+        wait(for: [exp], timeout: 5)
+    }
+
+    func testPOST() {
+        let exp = expectation(description: "Recieved response")
+        var request = URLRequest(url: URL(string: "http://localhost:8080")!)
+        request.httpMethod = "POST"
+        request.httpBody = "Hello world".data(using: .utf8)
+        request.setValue("text/html; charset=utf-8", forHTTPHeaderField: "Content-Type")
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            print(String(data: data ?? Data(), encoding: .utf8) ?? "nil")
+            print(response ?? "nil")
+            print(error ?? "nil")
+            if response != nil {
+                exp.fulfill()
+            }
+            }.resume()
+        wait(for: [exp], timeout: 5)
+    }
+
+    func testRoute() {
+        let exp = expectation(description: "Recieved response")
+        var request = URLRequest(url: URL(string: "http://localhost:8080/helloworld")!)
+        request.httpMethod = "GET"
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            print(String(data: data ?? Data(), encoding: .utf8) ?? "nil")
+            print(response ?? "nil")
+            print(error ?? "nil")
+            if response != nil {
+                exp.fulfill()
+            }
+            }.resume()
+        wait(for: [exp], timeout: 5)
+    }
+
+    func testDelay() {
+        try! server.stop()
+
+        server = Server(port: 8080, router: SwiftNIOMock.delay(.seconds(2), middleware: notFound))
+        try! server.start()
+
+        let exp = expectation(description: "Recieved response")
+        var request = URLRequest(url: URL(string: "http://localhost:8080")!)
+        request.httpMethod = "GET"
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            print(String(data: data ?? Data(), encoding: .utf8) ?? "nil")
+            print(response ?? "nil")
+            print(error ?? "nil")
+            if response != nil {
+                exp.fulfill()
+            }
+            }.resume()
         wait(for: [exp], timeout: 5)
     }
 
